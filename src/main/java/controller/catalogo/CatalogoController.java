cedfaf53f4bc2fde156867829663ce5632182071
aless <aless@host.docker.internal>
2025-12-10 14:34:39 +0100

    agggiustamento package

--- src/main/java/controller/catalogo/CatalogoController.java
+++ src/main/java/controller/catalogo/CatalogoController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controller.Catalogo;
+package controller.catalogo;
 
 import controller.DashboardController;
 import model.servizi.Catalogo;
