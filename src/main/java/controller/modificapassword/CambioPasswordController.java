d0d5522d6928f0af8d4f6baf9740adf774f19ed7
LuciaIas <l.iasevoli1@studenti.unisa.it>
2025-12-11 21:14:49 +0100

    Aggiornamento codice

--- src/main/java/controller/modificapassword/CambioPasswordController.java
+++ src/main/java/controller/modificapassword/CambioPasswordController.java
@@ -48,42 +48,39 @@ public class CambioPasswordController {
     private Label BtnAnnulla;
     
     @FXML
-    public void initialize(){
-        
-        setCheckBox();
-        setButtonFunction();
-        
-        
+    public void initialize(){       
+        setCheckBox();// Inizializza checkbox mostra/nascondi password
+        setButtonFunction();// Inizializza pulsanti Salva e Annulla       
     }
     
-    public void setButtonFunction(){
-    
+    public void setButtonFunction(){   
         BtnSalva.setOnAction(eh->{
             String pass,pass1;
-            if(!CheckShowPass.isSelected()){
-             pass = NewPass.getText();
+            if(!CheckShowPass.isSelected()){// Se la password non è visibile
+             pass = NewPass.getText();// Prende testo campo password
              pass1 = ConfirmPass.getText();
-            }else{
-            pass = NewPassVisible.getText();
+            }else{// Se la password è visibile
+            pass = NewPassVisible.getText();// Prende testo campo visibile
              pass1 = ConfirmPassVisible.getText();
             }
             
             //Controllo password e conferma password
-            if(pass.equals("") || pass1.equals("")){
+            if(pass.equals("") || pass1.equals("")){// Controllo se i campi sono vuoti
                 Alert err = new Alert(Alert.AlertType.WARNING);
                 err.setContentText("Completa entrambi i campi delle password");
                 err.showAndWait();
-                return;
-            }else if(!pass.equalsIgnoreCase(pass1)){
+                return;// Termina la funzione se campi vuoti
+                
+            }else if(!pass.equalsIgnoreCase(pass1)){ // Controlla corrispondenza password
                 Alert err = new Alert(Alert.AlertType.ERROR);
                 err.setContentText("Le password non corrispodono");
                 err.showAndWait();
                 return;
-            }else if(!ControlloFormato.controlloFormatoPassword(pass)){
-            
+                
+            }else if(!ControlloFormato.controlloFormatoPassword(pass)){           
                 Alert al = new Alert(AlertType.ERROR);
                 al.setTitle("Errore Validazione"); // Titolo della finestra
-                al.setHeaderText("Password non sicura 🔒"); // Titolo interno (o mettilo a null per toglierlo)
+                al.setHeaderText("Password non sicura 🔒"); // Titolo interno 
                 al.setContentText("La password deve avere:\n- Minimo 8 caratteri\n- Una maiuscola\n- Un numero\n- Un simbolo (@#$%^&+=!)");
 
                 DialogPane dialogPane = al.getDialogPane();
@@ -94,89 +91,62 @@ public class CambioPasswordController {
 
                 dialogPane.getStyleClass().add("my-alert");
 
-
                 al.showAndWait();
-                return;
-                
+                return;                
             }
-            
-          
-                DataBase.rimuoviBibliotecario();
-                DataBase.inserisciBibliotecario(pass);
-                 Alert IsbnAlert = new Alert(AlertType.INFORMATION);
-                IsbnAlert.setHeaderText("Password aggiornata");
-                IsbnAlert.setContentText("Modifica effettuata con successo");
+                // Aggiorna password nel database     
+                DataBase.rimuoviBibliotecario();// Rimuove vecchia password
+                DataBase.inserisciBibliotecario(pass);// Inserisce nuova password
+                 Alert IsbnAlert = new Alert(AlertType.INFORMATION); // Alert informativo
+                IsbnAlert.setHeaderText("Password aggiornata");// Alert interno
+                IsbnAlert.setContentText("Modifica effettuata con successo");// Contenuto alert
                 
                 DialogPane dialogPane = IsbnAlert.getDialogPane();
-
               
                 dialogPane.getStylesheets().add(
                     getClass().getResource("/CSS/StyleAccess.css").toExternalForm()
-                );
-
+                );                
+                dialogPane.getStyleClass().add("my-alert");                
+                IsbnAlert.showAndWait();// Mostra alert conferma
                 
-                dialogPane.getStyleClass().add("my-alert");
-                
-                IsbnAlert.showAndWait();
-                
-                Stage f =  (Stage) BtnSalva.getScene().getWindow();
-                f.close();
-            
-            
+                Stage f =  (Stage) BtnSalva.getScene().getWindow();// Ottiene finestra corrente
+                f.close();// Chiude finestra                     
         });
-        BtnAnnulla.setOnMouseClicked(eh->{
-        
-            Stage f =  (Stage) BtnAnnulla.getScene().getWindow();
-                f.close();
-            
-            
-        });
-        
+        BtnAnnulla.setOnMouseClicked(eh->{       
+            Stage f =  (Stage) BtnAnnulla.getScene().getWindow();// Ottiene finestra corrente
+                f.close();// Chiude finestra senza salvare                       
+        });        
     }
-    
-    
+       
     public void setCheckBox(){
-    showPassword(false);
-        CheckShowPass.setOnAction(eh->{
-        
+    showPassword(false);// Imposta password inizialmente nascosta
+        CheckShowPass.setOnAction(eh->{       
         if(CheckShowPass.isSelected())
-            showPassword(true);
+            showPassword(true);// Mostra password se selezionato
         else
-            showPassword(false);
-        
-        });
-        
+            showPassword(false);// Nasconde password se deselezionato       
+        });        
     }
-    
-    
-    public void showPassword(boolean yes){
         
-            if(yes){
-                NewPassVisible.setText(NewPass.getText());
-                NewPassVisible.setVisible(true);
-                NewPass.setVisible(false);
+    public void showPassword(boolean yes){       
+            if(yes){// Mostra password
+                NewPassVisible.setText(NewPass.getText());// Sincronizza testo
+                NewPassVisible.setVisible(true);// Mostra campo visibile
+                NewPass.setVisible(false);// Nasconde campo password
                 
                 ConfirmPassVisible.setText(ConfirmPass.getText());
                 ConfirmPassVisible.setVisible(true);
                 ConfirmPass.setVisible(false);
         
-            }else{
+            }else{// Nasconde password
                 NewPass.setText(NewPassVisible.getText());
-                NewPassVisible.setVisible(false);
-                NewPass.setVisible(true);
+                NewPassVisible.setVisible(false);// Nasconde campo visibile
+                NewPass.setVisible(true); // Mostra campo password
                 
                 ConfirmPass.setText(ConfirmPassVisible.getText());
                 ConfirmPassVisible.setVisible(false);
-                ConfirmPass.setVisible(true);
-            
-                
-            
-                        
-            }
-            
-            
-        
+                ConfirmPass.setVisible(true);               
+            }     
     }
-    
-    
+   
 }
